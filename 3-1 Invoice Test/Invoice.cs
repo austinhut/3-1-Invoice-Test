1bb11f8d3453df74bd1b920ce3569ef4d468aa1a
Austin <austin.hutmacher0113@omail.otech.edu>
2022-01-27 14:54:22 -0700

    Add project files.

---
+++ 3-1 Invoice Test/Invoice.cs
@@ -0,0 +1,19 @@
+﻿class Invoice
+{
+   private string partNumber; //part number instance variable
+   private string partDescription; //part description instance variable
+   private int partQty; //part quantity instance variable
+   private decimal partPrice;  //part price instance variable
+
+
+   //public Invoice()
+   //{
+   //   //partNumber = "9000187";
+   //   //partDescription = "Hammer";
+   //   //partQty = 4;
+   //   //partPrice = 12.99;
+}
+
+
+
+
---
+++ 3-1 Invoice Test/InvoiceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _3_1_Invoice_Test
+{
+   class InvoiceTest
+   {
+      static void Main()
+      {
+         Invoice item1 = new Invoice();
+
+         Console.Write("\nEnter the part's number: ");
+         string partNumber = (Console.ReadLine());
+
+         Console.Write("\nEnter the part's description: ");
+         string partDescription = (Console.ReadLine());
+
+         Console.Write("\nEnter the part's quantity: ");
+         string partQty = (Console.ReadLine());
+
+         Console.Write("\nEnter the part's price: ");
+         string partPrice = (Console.ReadLine());
+
+         Console.Write($"\nPart Number: {partNumber}");
+         Console.Write($"\nPart Description: {partDescription}");
+         Console.Write($"\nPart Quantity: {partQty}");
+         Console.Write($"\nPart Part Price: {partPrice}");
+         Console.Write($"\nTotal Invoice Price: {partPrice} * {partQty}");
+
+      }
+   }
+}
